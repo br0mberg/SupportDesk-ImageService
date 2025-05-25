78ee5fdbf56c55eef556b54d103e0a17c50ec132
Brombin Andrey <64542787+br0mberg@users.noreply.github.com>
2025-05-25 03:48:18 +0300

    Update KafkaImageConsumer.java

--- src/main/java/ru/brombin/image_service/service/kafka/KafkaImageConsumer.java
+++ src/main/java/ru/brombin/image_service/service/kafka/KafkaImageConsumer.java
@@ -2,6 +2,7 @@ package ru.brombin.image_service.service.kafka;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import ru.brombin.image_service.dto.DeleteImageRequest;
+import org.springframework.kafka.support.Acknowledgment;
 
 public interface KafkaImageConsumer {
     void consumeDeleteImageRequest(ConsumerRecord<String, DeleteImageRequest> record, , Acknowledgment ack);
